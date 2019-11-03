38dab9154fe2a8e78bf7009cb15e4981501bb372
nddalal <52607324+nddalal@users.noreply.github.com>
2019-11-03 15:21:34 -0500

    Add files via upload

--- graphics.py
+++ graphics.py
@@ -14,12 +14,13 @@ import time,random
 
 def appStarted(app):
     app.width, app.height = 10000, 10000
-    app.w,app.h= 100, 100
+    app.w,app.h= 80, 80
     app.colors=4
     app.color=set(i for i in range(app.colors+1))
     app.grid=([[random.randint(0,1)for j in range(app.w//2)] + [2*random.randint(0,1)for j in range(app.w//2)] for i in range(app.h//2)]
     + [[3*random.randint(0,1)for j in range(app.w//2)] + [4*random.randint(0,1)for j in range(app.w//2)] for i in range(app.h//2)])
     app.timerDelay = 50
+    app.started = False
 
 def bruh(n):
     a=[1,1,1,0,-1,-1,-1,0]
@@ -39,17 +40,22 @@ def ruleset(cell,neigh, app):
     else:
         if numAlive<2 or numAlive>3:return 0
         else:return cell
+        
+def keyPressed(app, event):
+    if event.key == 'Space':
+        app.started = True
 
 def timerFired(app):
-    newgrid=[[0]*app.w for i in range(app.h)]
-    for i in range(app.h):
-        for j in range(app.w):
-            near=[]
-            for k in range(8):
-                near.append(app.grid[(i+bruh(k)[0])%app.h][(j+bruh(k)[1])%
-                                     app.w])
-            newgrid[i][j]=ruleset(app.grid[i][j],near, app)
-    app.grid = newgrid
+    if app.started == True:
+        newgrid=[[0]*app.w for i in range(app.h)]
+        for i in range(app.h):
+            for j in range(app.w):
+                near=[]
+                for k in range(8):
+                    near.append(app.grid[(i+bruh(k)[0])%app.h][(j+bruh(k)[1])%
+                                         app.w])
+                newgrid[i][j]=ruleset(app.grid[i][j],near, app)
+        app.grid = newgrid
                 
 def drawBoard(app, canvas): #loops through board and draws cells for row & col
     for row in range(len(app.grid)):
@@ -68,13 +74,17 @@ def drawCell(app, canvas, row, col):
                             fill = color, outline = color)
     
 def redrawAll(app, canvas):
-    drawBoard(app, canvas)
-    
+    if app.started == True:
+        drawBoard(app, canvas)
+    else:
+        canvas.create_rectangle(0, 0, app.width, app.height, 
+                                fill = 'black', outline = 'black')
+        canvas.create_text(app.width/2, app.height/2, text = 'Welcome to Quad Life', 
+                           font = 'Arial 36 bold',fill = 'white')
+        canvas.create_text(app.width/2, app.height/2 + 80 , text = 'Press Space to Start', 
+                           font = 'Arial 24',fill = 'white')
+
 
 runApp(width = 2000, height = 2000)
     
     
-
-
-
-
